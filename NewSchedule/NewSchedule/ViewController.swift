637bad57f4a70db691d31d8db2e336ef3aa53b8b
Timofey <timaluk@icloud.com>
2020-02-23 17:27:53 +0300

    add parsing and iOS <12 support

--- NewSchedule/NewSchedule/ViewController.swift
+++ NewSchedule/NewSchedule/ViewController.swift
@@ -8,6 +8,14 @@
 
 import UIKit
 
+// EXAMPLE HOW TO USE COLLEGE DATA
+
+//RequestKBP.getData(stringURL: "https://kbp.by/rasp/timetable/view_beta_kbp/?cat=group&id=31")
+//RequestKBP.dispGroup.wait()
+//print(RequestKBP.PointsOfChanges ?? "ERROR")
+//for pair in (RequestKBP.Curriculum!) {
+//    print(pair?.pairName ?? "ERROR 2")
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
